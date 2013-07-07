401ff511666ed36e561846dfc770821c830011b3
Ryan Sciampacone <rsciampacone@gmail.com>
2013-07-06 21:36:24 -0400

    Revamp logging mechanism to a module with bool return values for type

--- simpleserver.rb
+++ simpleserver.rb
@@ -1,43 +1,62 @@
 require 'socket'
 require 'thread' #using threads and older Ruby needs this for Mutex
 
-class ParsingError < StandardError
-end
-
-class CommandError < StandardError
+module Logger
+	def self.init_logging(io)
+		# First initializer decides what the IO is for logging output
+		@@io ||= io
+		@@mutex ||= Mutex.new
+	end
+	
+	def log_info(message)
+		log("INFO: " + message)
+		true
+	end
+	
+	def log_error(message)
+		log("ERROR: " + message)
+		false
+	end
+	
+	private
+	
+	def log(message)
+		@@mutex.synchronize {
+			@@io.puts(message)
+		}
+	end
+	
 end
 
 class CommandHandler
+	include Logger
+
 	def initialize(datastore)
 		@mutex = Mutex.new
 		@datastore = datastore
 	end
 
-	def dispatch(connection, statement)
+	def dispatch(statement, connection)
 			method = ("cmd_" + statement.first.downcase).to_sym
 			arguments = statement.slice(1..-1)
-			puts "Dispatching \"#{method}\" with arguments (#{arguments.class}):" #DEBUG
-			puts arguments #DEBUG
+			log_info("Client #{connection} dispatching #{method} => #{statement.to_s}")
 			
 			@mutex.synchronize {
 				self.send(method, connection, *arguments)
 			}
 	end
 	
+	def log_cmd(command_name, args)
+		log_info("Command #{command_name} => #{args.to_s}")
+	end
+
 	# Handling the case where a command is sent that isn't actually understood
 	def method_missing(key, *args)
-		raise CommandError, "Command \"#{key}\" is not understood with arguments => #{args}"
+		return log_error("Command \"#{key}\" is not understood with arguments => #{args.to_s}") if key =~ /^cmd_/
+		super.method_missing(key, *args)
 	end
 	
 	private
-	
-	#DEBUG
-	def debug_loc(location, args)
-		puts "Got to the #{location} command"
-		args.each_with_index do |x, i|
-			puts "#{i} => #{x}"
-		end
-	end
 	
 	#TODO reply_ messages probably should just return the string and this gets handled at the server level
 	def reply_ok(connection)
@@ -57,82 +76,84 @@ class CommandHandler
 	end
 
 	def cmd_(connection, *args)
-		#do nothing
-		debug_loc("<NULL>", args)
 		reply_error(connection, "unknown command ''")
 	end
 	alias cmd_null cmd_
 
 	def cmd_set(connection, *args)
-		debug_loc("SET", args)
-		raise CommandError, "SET command did not receive key/value pair #{args}" if args.length < 2	
-		#TODO Handle the special case commands
-		raise CommandError, "SET command had more than a key/value pair #{args}" if args.length > 2
+		#TODO Handle special case or extended commands for "SET"
+		return log_error("SET command did not receive key/value pair #{args.to_s}") if args.length < 2	
+		return log_error("SET command had more than a key/value pair #{args.to_s}") if args.length > 2
 
 		@datastore[args[0].to_sym] = args[1]
-
+		log_cmd("SET", args)
 		reply_ok(connection)
 	end
 	
 	def cmd_get(connection, *args)
-		debug_loc("GET", args)
-		raise CommandError, "GET command received more than a single key #{args}" if args.length != 1
+		log_error("GET command received more than a single key #{args.to_s}") if args.length != 1
 		
 		value = @datastore[args[0].to_sym]		
+		log_cmd("GET", args)
 		reply_bulk(connection, value)
 	end
 end
 
 class CommandParser
+	include Logger
+
 	def parse_length(leading, arg_to_parse)
-		return false if arg_to_parse.nil?
-		raise ParsingError, "Client argument didn't end in \\r\\n => \"#{arg}\"" if arg_to_parse[-2..-1] != "\r\n"
+		return log_error("Arguments to parse were \"nil\"") if arg_to_parse.nil?
+		return log_error("Client argument didn't end in \\r\\n => \"#{arg}\"") if arg_to_parse[-2..-1] != "\r\n"
+
 		arg = arg_to_parse.chomp("\r\n")
-		raise ParsingError, "Client argument length did not begin with \"$\": \"#{arg}\"" if not arg.start_with? leading
-		raise ParsingError, "Client argument length contained no value \"#{arg}\"" if arg.length < 1
+		
+		return log_error("Client argument length did not begin with \"$\": \"#{arg}\"") if not arg.start_with? leading
+		return log_error("Client argument length contained no value \"#{arg}\"") if arg.length < 1
+
 		begin
 			Integer(arg[1..-1])
 		rescue ArgumentError => e
-			raise ParsingError, "Client command count not a valid integer format \"#{arg}\""
+			log_error("Client command count not a valid integer format \"#{arg}\"")
 		end
 	end
 	
 	def parse(connection)
-		begin
+		line = connection.gets()
+		count = parse_length("*", line)
+		return false if not count
+		log_info("Bulk command count \"#{line.chomp("\r\n")}\" => #{count}")
+
+		statement = []
+		index = 0	
+		while index < count
 			line = connection.gets()
-			count = parse_length("*", line)
-			return false if not count
-			puts "\"#{line.chomp("\r\n")}\" => #{count}" #DEBUG
+			length = parse_length("$", line)
+			return false if not length
+			log_info("Arg#{index} length \"#{line.chomp("\r\n")}\" => #{length}")
 
-			statement = []			
-			while count > 0
-				line = connection.gets()
-				length = parse_length("$", line)
-				return false if not length
-				puts "\"#{line.chomp("\r\n")}\" => #{length}" #DEBUG
+			argument = connection.gets()
+			return false if argument == nil
+			argument.chomp!("\r\n")
+			log_info("Arg#{index} \"#{argument}\"")
 
-				argument = connection.gets()
-				return false if argument == nil
-				argument.chomp!("\r\n")
-				puts "\"#{argument}\" => #{argument}"  #DEBUG
+			return log_error("Argument described length does not match text length #{length} => #{argument}") if argument.length != length
 
-				raise ParsingError, "Argument described length does not match text length #{length} => #{argument}" if argument.length != length
+			statement << argument
 
-				statement << argument
-
-				count -= 1
-			end
-
-			# TODO Should this be an error?  Need to check if the server can take *0 as a statement
-			statement << "null" if statement.empty?
-			statement
-		rescue Errno::ECONNRESET => e
-			raise ParsingError, "BASE HANDLER: socket seemed to have died: #{e.to_s}"
+			index += 1
 		end
+
+		# TODO Should this be an error?  Need to check if the server can take *0 as a statement
+		statement << "null" if statement.empty?
+		statement
 	end
 end
 
 class Server
+	include Logger
+	Logger::init_logging(STDOUT)
+
 	def initialize
 		@datastore = Hash.new
 		@parser = CommandParser.new
@@ -144,22 +165,18 @@ class Server
 		while true
 			connection = port.accept
 			Thread.new do
-				puts "Client connected to server"
+				log_info("Client #{connection} connected to server")
 				begin
 					while statement = @parser.parse(connection)
-						puts "Command parsed successfully"
-						@handler.dispatch connection, statement
+						@handler.dispatch(statement, connection)
 					end
 				rescue Errno::ECONNRESET => e
-					puts "Exception on socket: #{e.to_s}"
-				rescue ParsingError => e
-					puts "Actually got a parsing error raised!!!"
-					puts e.to_s
-					puts $ERROR_POSITION
+					log_error("Exception on socket: #{e.to_s}")
 				rescue Exception => e
-					puts "Something unexpected happend: #{e.class} => #{e.to_s}"
+					log_error("Something unexpected happend: #{e.class} => #{e.to_s}")
 				end
-				puts "Server thread terminating"
+				log_info("Client #{connection} terminating")
+				connection.close
 			end
 		end
 	end
